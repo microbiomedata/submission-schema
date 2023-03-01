b0203a64362b2c6db6a1c1eddf39e13a074e4bc2
Mark A. Miller <mamillerpa@gmail.com>
2023-03-01 11:34:15 -0500

    chore: initial commit

--- src/submission_schema/datamodel/submission_schema.py
+++ src/submission_schema/datamodel/submission_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from submission_schema.yaml by pythongen.py version: 0.9.0
-# Generation date: 2023-03-01T11:33:33
+# Generation date: 2023-03-01T11:34:11
 # Schema: submission-schema
 #
 # id: https://w3id.org/microbiomedata/submission-schema
