539354b6bcef0ffb6ef8dfd6cc5400245828eda9
Patrick Kalita <pkalita@lbl.gov>
2025-01-24 13:34:31 -0800

    Update src/nmdc_submission_schema/scripts/export_nmdc_schema.py
    Co-authored-by: eecavanna <134325062+eecavanna@users.noreply.github.com>

--- src/nmdc_submission_schema/scripts/export_nmdc_schema.py
+++ src/nmdc_submission_schema/scripts/export_nmdc_schema.py
@@ -6,6 +6,9 @@ import click
 @click.command()
 @click.option('--output-file', '-o', type=click.File('w'), default="-")
 def main(output_file):
+    r"""
+    Dumps a YAML representation of the NMDC Schema to the specified file.
+    """
     schema_file_name = 'nmdc_materialized_patterns.yaml'
     with importlib.resources.open_text('nmdc_schema', schema_file_name) as input_file:
         for line in input_file:
