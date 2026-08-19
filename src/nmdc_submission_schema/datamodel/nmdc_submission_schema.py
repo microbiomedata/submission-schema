1514cd5cc61739fa8d4c82cd70a6bba2a47af828
Patrick Kalita <pkalita@lbl.gov>
2026-08-19 10:11:15 -0700

    11.23.1

--- src/nmdc_submission_schema/datamodel/nmdc_submission_schema.py
+++ src/nmdc_submission_schema/datamodel/nmdc_submission_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc_submission_schema.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-08-13T17:02:42
+# Generation date: 2026-08-19T10:09:47
 # Schema: nmdc_submission_schema
 #
 # id: https://example.com/nmdc_submission_schema
